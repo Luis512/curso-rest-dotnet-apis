511e70794df733b9a4cc508753784bb368335beb
Luis Alvarez <luis.eav@hotmail.com>
2020-07-16 20:52:01 -0600

    Sesion-04: WebServer

--- material/WebApi/Startup.cs
+++ material/WebApi/Startup.cs
@@ -18,7 +18,12 @@ namespace WebApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {
+            services.AddCors(opt => opt.AddDefaultPolicy(builder => {
+                builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+            }));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
@@ -41,6 +46,8 @@ namespace WebApi
                 c.RoutePrefix = "api";
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseCors();
 
             app.UseHttpsRedirection();
 
